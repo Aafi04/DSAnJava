d920cd33e5dcdc018da417531703817e5448a1c7
Aafi04 <mdaafi04@gmail.com>
2025-04-10 09:08:51 +0530

    Reverse of an integer.

---
+++ reverseNum.java
@@ -0,0 +1,13 @@
+public class reverseNum {
+    public static void main(String[] args) {
+        int num = 74727731;
+        int result = 0;
+        int ld;
+        for (int i = 0; num > 0; i++) {
+            ld = num % 10; //ld becomes 1
+            num /= 10; //becomes 7472773
+            result = result * 10 + ld; //result becomes 0*10 + 1
+        }
+        System.out.println(result);
+    }
+}
