ac4637a5a62fd5efb6a17a10bd6bf95fd4e020dc
Aafi04 <mdaafi04@gmail.com>
2025-04-12 15:48:23 +0530

    An easy leetcode problem

---
+++ Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int original = x;
+        int ld;
+        int temp = 0;
+        for (int i = 0; x > 0; i++) {
+            ld = x % 10;
+            temp = (temp * 10) + ld;
+            x /= 10;
+        }
+        return original == temp;
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        boolean result = solution.isPalindrome(121);
+        System.out.println(result);
+    }
+}
