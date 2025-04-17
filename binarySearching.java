02dd9596f64b38dfb331fbe6f27f3c109f23b295
Aafi04 <mdaafi04@gmail.com>
2025-04-17 10:30:20 +0530

    Improved binary search for both ascending and descending sorted arrays.

--- binarySearching.java
+++ binarySearching.java
@@ -1,7 +1,7 @@
 public class binarySearching{
     public static void main(String[] args) {
-        int[] arr = {124, 157, 176, 198, 207, 209, 384};
-        int target = 157;
+        int[] arr = {384, 357, 306, 257, 201, 198, 154, 20};
+        int target = 357;
         System.out.println(binarySearch(arr, target));
 
     }
@@ -9,29 +9,61 @@ public class binarySearching{
     static int binarySearch(int[] arr, int target) {
         int starti = 0;
         int endi = arr.length - 1;
-        int midi = (starti + endi) / 2;
+        int midi;
 
-        while (starti <= endi) {
 
-            if (target < arr[midi]) {
-                endi = midi - 1;
+        if (arr[0] < arr[arr.length - 1]) {
+            while (starti <= endi) {
+
                 midi = (starti + endi) / 2;
-            }
-            
-            else if (target > arr[midi]) {
-                starti = midi + 1;
-                midi = (starti + endi) / 2;
-            }
 
-            else if (target == arr[midi]) {
-                System.out.println("Element found at: " + midi);
-            }
+                if (target < arr[midi]) {
+                    endi = midi - 1;
+                }
 
-            else {
-                System.out.println("Element not found");
-            }
+                else if (target > arr[midi]) {
+                    starti = midi + 1;
+                }
 
+                else if (target == arr[midi]) {
+                    System.out.println("Element found at: ");
+                    return midi;
+                }
+
+                else {
+                    System.out.println("Element not found");
+                }
+
+            }
+            return -1;
         }
-        return -1;
+
+        else {
+            while (starti <= endi) {
+
+                midi = (starti + endi) / 2;
+
+                if (target < arr[midi]) {
+                    starti = midi + 1;
+                }
+
+                else if (target > arr[midi]) {
+                    endi = midi - 1;
+                }
+
+                else if (target == arr[midi]) {
+                    System.out.println("Element found at: ");
+                    return midi;
+                }
+
+                else {
+                    System.out.println("Element not found");
+                }
+
+            }
+            return -1;
+        }
+
+
     }
 }
