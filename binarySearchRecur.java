4e67c1855cc96cb2945c38b1dab73101a4fa8b6e
Aafi04 <mdaafi04@gmail.com>
2025-04-20 22:28:34 +0530

    Binary search using recursion

---
+++ binarySearchRecur.java
@@ -0,0 +1,31 @@
+public class binarySearchRecur {
+
+    public static int binarySearch(int[] arr, int target, int left, int right) {
+        if (left > right) {
+            return -1;
+        }
+
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        } else if (arr[mid] > target) {
+            return binarySearch(arr, target, left, mid - 1);
+        } else {
+            return binarySearch(arr, target, mid + 1, right);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 6, 8, 10, 12, 14};
+        int target = 10;
+
+        int result = binarySearch(arr, target, 0, arr.length - 1);
+
+        if (result != -1) {
+            System.out.println("Element " + target + " found at index: " + result);
+        } else {
+            System.out.println("Element " + target + " not found in the array.");
+        }
+    }
+}
